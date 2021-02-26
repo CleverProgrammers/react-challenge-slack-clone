0e5fd61719fc5d52a9c82226620844c36be2b1f4
Ale <alearanibar@gmail.com>
2021-02-25 23:21:09 -0800

    fixed sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,75 +1,59 @@
-import React from 'react'
-import styled from 'styled-components'
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-import { sidebarItemsData } from '../data/SidebarData'
-import { ChannelData } from '../data/ChannelData'
-import AddIcon from '@material-ui/icons/Add';
+import React from "react";
+import styled from "styled-components";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import { sidebarItemsData } from "../data/SidebarData";
+import { ChannelData } from "../data/ChannelData";
+import AddIcon from "@material-ui/icons/Add";
 
 function Sidebar() {
-    return (
-        <Container>
-            <WorkspaceContainer>
-                <Name>
-                    CleverProgrammer
-                </Name>
-                <NewMessage>
-                    <AddCircleOutlineIcon />
-                </NewMessage>
-            </WorkspaceContainer>
-            <MainChannels>
-                {
-                    sidebarItemsData.map(item => (
-                        <MainChannelItem>
-                            {item.icon}
-                            {item.text}
-                        </MainChannelItem>
-                    ))
-                }
-
-            </MainChannels>
-            <ChannelsContainer>
-                <NewChannelContainer>
-                    <div>
-                        Channels
-                    </div>
-                    <AddIcon />
-                </NewChannelContainer>
-                <ChannelsList>
-                    {
-                        ChannelData.map((item, i) => {
-                            return (
-                                <Channel>
-                                    # {item.name}
-                                </Channel>
-                            )
-                        })
-                    }
-                    
-                </ChannelsList>
-            </ChannelsContainer>
-            
-        </Container>
-    )
+  return (
+    <Container>
+      <WorkspaceContainer>
+        <Name>CleverProgrammer</Name>
+        <NewMessage>
+          <AddCircleOutlineIcon />
+        </NewMessage>
+      </WorkspaceContainer>
+      <MainChannels>
+        {sidebarItemsData.map((item) => (
+          <MainChannelItem>
+            {item.icon}
+            {item.text}
+          </MainChannelItem>
+        ))}
+      </MainChannels>
+      <ChannelsContainer>
+        <NewChannelContainer>
+          <div>Channels</div>
+          <AddIcon />
+        </NewChannelContainer>
+        <ChannelsList>
+          {ChannelData.map((item, i) => {
+            return <Channel># {item.name}</Channel>;
+          })}
+        </ChannelsList>
+      </ChannelsContainer>
+    </Container>
+  );
 }
 
-export default Sidebar
-
+export default Sidebar;
 
 const Container = styled.div`
   background: #016d7e;
 `;
 
 const WorkspaceContainer = styled.div`
-    color: white;
-    height: 64px;
-    display: flex;
-    align-items: center;
-    padding-left: 19px;
-    justify-content: space-between;
-    border-bottom: 1px solid  #532753;
-`
+  color: white;
+  height: 64px;
+  display: flex;
+  align-items: center;
+  padding-left: 19px;
+  justify-content: space-between;
+  border-bottom: 1px solid #532753;
+`;
 
-const Name = styled.div``
+const Name = styled.div``;
 
 const NewMessage = styled.div`
   width: 36px;
@@ -86,45 +70,45 @@ const NewMessage = styled.div`
 `;
 
 const MainChannels = styled.div`
-    padding-top: 20px;
-`
+  padding-top: 20px;
+`;
 
 const MainChannelItem = styled.div`
-    color: rgb(188,171,188);
-    display: grid;
-    grid-template-columns: 15% auto;
-    height: 28px;
-    align-items: center;
-    padding-left: 19px;
-    cursor: pointer;
-    :hover {
-        background: #350D36;
-    }
-`
+  color: rgb(188, 171, 188);
+  display: grid;
+  grid-template-columns: 15% auto;
+  height: 28px;
+  align-items: center;
+  padding-left: 19px;
+  cursor: pointer;
+  :hover {
+    background: #350d36;
+  }
+`;
 
 const ChannelsContainer = styled.div`
-    color: rgb(188,171,188);
-    margin-top: 10px;
-`
+  color: rgb(188, 171, 188);
+  margin-top: 10px;
+`;
 
 const NewChannelContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 28px;
-    padding-left: 19px;
-    padding-right: 12px;
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 28px;
+  padding-left: 19px;
+  padding-right: 12px;
+`;
 
-const ChannelsList = styled.div``
+const ChannelsList = styled.div``;
 
 const Channel = styled.div`
-    height: 28px;
-    display: flex;
-    align-items: center;
-    padding-left: 19px;
-    cursor: pointer;
-    :hover {
-        background: #350D36;
-    }
-`
+  height: 28px;
+  display: flex;
+  align-items: center;
+  padding-left: 19px;
+  cursor: pointer;
+  :hover {
+    background: #350d36;
+  }
+`;
